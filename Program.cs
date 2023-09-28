a0c7fa1477ac03fd48dc1862e6e515226b3454fa
wesleyruchel <wesleyruchel@gmail.com>
2023-09-28 20:13:33 -0300

    Handle multiple connections

--- Program.cs
+++ Program.cs
@@ -10,36 +10,67 @@ var app = builder.Build();
 
 app.UseWebSockets();
 
+var connections = new List<WebSocket>();
+
 app.Map("/", async context =>
 {
-
     if (!context.WebSockets.IsWebSocketRequest)
     {
         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
     }
     else
     {
+        var username = context.Request.Query["username"];
         using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        connections.Add(webSocket);
 
-        while (true)
+        await Broadcast($"{username} joined");
+        await Broadcast($"{connections.Count} users connected");
+        await ReceiveMessage(webSocket, async (receive, buffer) =>
         {
-            var message = "The current time is: " + DateTime.Now.ToString("HH:mm:ss");
-            var bytes = Encoding.UTF8.GetBytes(message);
+            if (receive.MessageType == WebSocketMessageType.Text)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, receive.Count);
+
+                await Broadcast(username + ": " + message);
+            }
+            else if (receive.MessageType == WebSocketMessageType.Close || webSocket.State == WebSocketState.Aborted)
+            {
+                connections.Remove(webSocket);
+
+                await Broadcast($"{username} left");
+                await Broadcast($"{connections.Count} users connected");
+                await webSocket.CloseAsync(receive.CloseStatus.Value, receive.CloseStatusDescription, CancellationToken.None);
+            }
+        });
+    }
+});
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+{
+    var buffer = new byte[1024 * 6];
+
+    while (socket.State == WebSocketState.Open)
+    {
+        var receive = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        handleMessage(receive, buffer);
+    }
+}
+
+async Task Broadcast(string message)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+
+    foreach (var socket in connections)
+    {
+        if (socket.State == WebSocketState.Open)
+        {
             var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
 
-            if (webSocket.State == WebSocketState.Open)
-            {
-                await webSocket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
-            }
-            else if (webSocket.State == WebSocketState.Closed || webSocket.State == WebSocketState.Aborted)
-            {
-                break;
-            }
-            Thread.Sleep(1000);
-
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
         }
     }
-
-});
+}
 
 await app.RunAsync();
